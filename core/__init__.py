56032c156a95e441e7530e903582f2bc2e21fc10
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-06 14:37:32 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,15 @@
+"""
+MarketVerse Core Package
 
+Core engine initialization.
+"""
+
+from .bootstrap import Bootstrap
+from .startup import StartupManager
+from .engine import MarketVerseEngine
+
+__all__ = [
+    "Bootstrap",
+    "StartupManager",
+    "MarketVerseEngine",
+]
