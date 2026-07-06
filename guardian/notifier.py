29efef3470b7e1a575c5cceb98bd9e8e3fd08fc9
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-06 14:01:47 +0530

    Update notifier.py

--- guardian/notifier.py
+++ guardian/notifier.py
@@ -1,1 +1,42 @@
-# TODO
+"""
+MarketVerse Guardian
+notifier.py
+
+Purpose:
+Generate project notifications.
+"""
+
+
+class Notification:
+
+    def __init__(self, level, title, message):
+        self.level = level
+        self.title = title
+        self.message = message
+
+    def __str__(self):
+        return f"[{self.level}] {self.title}: {self.message}"
+
+
+class Notifier:
+
+    def info(self, message):
+        return Notification(
+            "INFO",
+            "Information",
+            message
+        )
+
+    def warning(self, message):
+        return Notification(
+            "WARNING",
+            "Warning",
+            message
+        )
+
+    def error(self, message):
+        return Notification(
+            "ERROR",
+            "Error",
+            message
+        )
