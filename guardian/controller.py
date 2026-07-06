099e9b830ca2be0592b5328d5e9f77e343505d7d
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-06 14:23:44 +0530

    Update controller.py

--- guardian/controller.py
+++ guardian/controller.py
@@ -1,1 +1,43 @@
-# TODO
+"""
+MarketVerse Guardian
+controller.py
+
+Purpose:
+Coordinate all Guardian components.
+"""
+
+from .scanner import ProjectScanner
+from .validator import ProjectValidator
+from .dependency import DependencyAnalyzer
+from .health import HealthMonitor
+from .advisor import GuardianAdvisor
+
+
+class GuardianController:
+
+    def __init__(self):
+        self.scanner = ProjectScanner()
+        self.validator = ProjectValidator()
+        self.dependency = DependencyAnalyzer()
+        self.health = HealthMonitor()
+        self.advisor = GuardianAdvisor()
+
+    def run(self, root="."):
+        files = self.scanner.scan(root)
+
+        results = [
+            self.validator.validate(file)
+            for file in files
+        ]
+
+        report = self.health.generate(
+            files,
+            results
+        )
+
+        advice = self.advisor.advise(report)
+
+        return {
+            "report": report,
+            "advice": advice
+        }
