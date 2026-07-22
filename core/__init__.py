a278175bf358b3119df5aa690fe8e11014066c31
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-22 23:37:23 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -7,19 +7,15 @@ Core engine initialization.
 from .bootstrap import Bootstrap
 from .startup import StartupManager
 from .engine import MarketVerseEngine
-from marketverse.guardian import GuardianController
+from guardian.controller import GuardianController
+
 __all__ = [
     "Bootstrap",
     "StartupManager",
     "MarketVerseEngine",
 ]
+
 def run_guardian():
     """Run Guardian system checks."""
-
     controller = GuardianController()
-
     return controller.run()
-
-self.advisor = GuardianAdvisor()
-self.import_checker = ImportChecker()
-self.notifier = Notifier()
