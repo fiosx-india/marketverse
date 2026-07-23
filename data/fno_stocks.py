87b73410f230025abd982909c468734335bf48a2
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-23 09:24:36 +0530

    Update fno_stocks.py

--- data/fno_stocks.py
+++ data/fno_stocks.py
@@ -3,67 +3,46 @@
 # ==========================================
 
 FNO_STOCKS = [
+    {"name": "Reliance Industries Ltd", "symbol": "RELIANCE.NS", "sector": "Energy"},
+    {"name": "HDFC Bank Ltd", "symbol": "HDFCBANK.NS", "sector": "Banking"},
+    {"name": "ICICI Bank Ltd", "symbol": "ICICIBANK.NS", "sector": "Banking"},
+    {"name": "State Bank of India", "symbol": "SBIN.NS", "sector": "Banking"},
+    {"name": "Axis Bank Ltd", "symbol": "AXISBANK.NS", "sector": "Banking"},
+    {"name": "Kotak Mahindra Bank Ltd", "symbol": "KOTAKBANK.NS", "sector": "Banking"},
+    {"name": "IndusInd Bank Ltd", "symbol": "INDUSINDBK.NS", "sector": "Banking"},
+    {"name": "Punjab National Bank", "symbol": "PNB.NS", "sector": "Banking"},
 
-    {
-        "name": "Reliance Industries",
-        "symbol": "RELIANCE.NS",
-        "sector": "Energy"
-    },
+    {"name": "Infosys Ltd", "symbol": "INFY.NS", "sector": "IT"},
+    {"name": "Tata Consultancy Services", "symbol": "TCS.NS", "sector": "IT"},
+    {"name": "HCL Technologies", "symbol": "HCLTECH.NS", "sector": "IT"},
+    {"name": "Wipro Ltd", "symbol": "WIPRO.NS", "sector": "IT"},
+    {"name": "Tech Mahindra", "symbol": "TECHM.NS", "sector": "IT"},
 
-    {
-        "name": "TCS",
-        "symbol": "TCS.NS",
-        "sector": "Information Technology"
-    },
+    {"name": "Larsen & Toubro", "symbol": "LT.NS", "sector": "Infrastructure"},
+    {"name": "Bharti Airtel", "symbol": "BHARTIARTL.NS", "sector": "Telecom"},
+    {"name": "Tata Motors", "symbol": "TATAMOTORS.NS", "sector": "Automobile"},
+    {"name": "Maruti Suzuki", "symbol": "MARUTI.NS", "sector": "Automobile"},
+    {"name": "Mahindra & Mahindra", "symbol": "M&M.NS", "sector": "Automobile"},
+    {"name": "Bajaj Auto", "symbol": "BAJAJ-AUTO.NS", "sector": "Automobile"},
+    {"name": "Eicher Motors", "symbol": "EICHERMOT.NS", "sector": "Automobile"},
 
-    {
-        "name": "Infosys",
-        "symbol": "INFY.NS",
-        "sector": "Information Technology"
-    },
+    {"name": "Sun Pharmaceutical", "symbol": "SUNPHARMA.NS", "sector": "Pharma"},
+    {"name": "Dr. Reddy's Laboratories", "symbol": "DRREDDY.NS", "sector": "Pharma"},
+    {"name": "Cipla Ltd", "symbol": "CIPLA.NS", "sector": "Pharma"},
+    {"name": "Divi's Laboratories", "symbol": "DIVISLAB.NS", "sector": "Pharma"},
 
-    {
-        "name": "HDFC Bank",
-        "symbol": "HDFCBANK.NS",
-        "sector": "Banking"
-    },
+    {"name": "NTPC Ltd", "symbol": "NTPC.NS", "sector": "Power"},
+    {"name": "Power Grid Corporation", "symbol": "POWERGRID.NS", "sector": "Power"},
+    {"name": "ONGC Ltd", "symbol": "ONGC.NS", "sector": "Oil & Gas"},
+    {"name": "BPCL Ltd", "symbol": "BPCL.NS", "sector": "Oil & Gas"},
+    {"name": "Coal India Ltd", "symbol": "COALINDIA.NS", "sector": "Mining"},
 
-    {
-        "name": "ICICI Bank",
-        "symbol": "ICICIBANK.NS",
-        "sector": "Banking"
-    },
+    {"name": "Tata Steel", "symbol": "TATASTEEL.NS", "sector": "Metals"},
+    {"name": "JSW Steel", "symbol": "JSWSTEEL.NS", "sector": "Metals"},
+    {"name": "Hindalco Industries", "symbol": "HINDALCO.NS", "sector": "Metals"},
 
-    {
-        "name": "State Bank of India",
-        "symbol": "SBIN.NS",
-        "sector": "Banking"
-    },
-
-    {
-        "name": "Larsen & Toubro",
-        "symbol": "LT.NS",
-        "sector": "Infrastructure"
-    },
-
-    {
-        "name": "ITC",
-        "symbol": "ITC.NS",
-        "sector": "FMCG"
-    },
-
-    {
-        "name": "Bharti Airtel",
-        "symbol": "BHARTIARTL.NS",
-        "sector": "Telecom"
-    },
-
-    {
-        "name": "Tata Motors",
-        "symbol": "TATAMOTORS.NS",
-        "sector": "Automobile"
-    }
-
+    {"name": "Adani Enterprises", "symbol": "ADANIENT.NS", "sector": "Diversified"},
+    {"name": "Adani Ports", "symbol": "ADANIPORTS.NS", "sector": "Ports"},
 ]
 
 # ==========================================
@@ -121,46 +100,4 @@ def get_all_sectors():
             }
         )
     )
-
-FNO_STOCKS = [
-    {"name": "Reliance Industries Ltd", "symbol": "RELIANCE.NS", "sector": "Energy"},
-    {"name": "HDFC Bank Ltd", "symbol": "HDFCBANK.NS", "sector": "Banking"},
-    {"name": "ICICI Bank Ltd", "symbol": "ICICIBANK.NS", "sector": "Banking"},
-    {"name": "State Bank of India", "symbol": "SBIN.NS", "sector": "Banking"},
-    {"name": "Axis Bank Ltd", "symbol": "AXISBANK.NS", "sector": "Banking"},
-    {"name": "Kotak Mahindra Bank Ltd", "symbol": "KOTAKBANK.NS", "sector": "Banking"},
-    {"name": "IndusInd Bank Ltd", "symbol": "INDUSINDBK.NS", "sector": "Banking"},
-    {"name": "Punjab National Bank", "symbol": "PNB.NS", "sector": "Banking"},
-
-    {"name": "Infosys Ltd", "symbol": "INFY.NS", "sector": "IT"},
-    {"name": "Tata Consultancy Services", "symbol": "TCS.NS", "sector": "IT"},
-    {"name": "HCL Technologies", "symbol": "HCLTECH.NS", "sector": "IT"},
-    {"name": "Wipro Ltd", "symbol": "WIPRO.NS", "sector": "IT"},
-    {"name": "Tech Mahindra", "symbol": "TECHM.NS", "sector": "IT"},
-
-    {"name": "Larsen & Toubro", "symbol": "LT.NS", "sector": "Infrastructure"},
-    {"name": "Bharti Airtel", "symbol": "BHARTIARTL.NS", "sector": "Telecom"},
-    {"name": "Tata Motors", "symbol": "TATAMOTORS.NS", "sector": "Automobile"},
-    {"name": "Maruti Suzuki", "symbol": "MARUTI.NS", "sector": "Automobile"},
-    {"name": "Mahindra & Mahindra", "symbol": "M&M.NS", "sector": "Automobile"},
-    {"name": "Bajaj Auto", "symbol": "BAJAJ-AUTO.NS", "sector": "Automobile"},
-    {"name": "Eicher Motors", "symbol": "EICHERMOT.NS", "sector": "Automobile"},
-
-    {"name": "Sun Pharmaceutical", "symbol": "SUNPHARMA.NS", "sector": "Pharma"},
-    {"name": "Dr. Reddy's Laboratories", "symbol": "DRREDDY.NS", "sector": "Pharma"},
-    {"name": "Cipla Ltd", "symbol": "CIPLA.NS", "sector": "Pharma"},
-    {"name": "Divi's Laboratories", "symbol": "DIVISLAB.NS", "sector": "Pharma"},
-
-    {"name": "NTPC Ltd", "symbol": "NTPC.NS", "sector": "Power"},
-    {"name": "Power Grid Corporation", "symbol": "POWERGRID.NS", "sector": "Power"},
-    {"name": "ONGC Ltd", "symbol": "ONGC.NS", "sector": "Oil & Gas"},
-    {"name": "BPCL Ltd", "symbol": "BPCL.NS", "sector": "Oil & Gas"},
-    {"name": "Coal India Ltd", "symbol": "COALINDIA.NS", "sector": "Mining"},
-
-    {"name": "Tata Steel", "symbol": "TATASTEEL.NS", "sector": "Metals"},
-    {"name": "JSW Steel", "symbol": "JSWSTEEL.NS", "sector": "Metals"},
-    {"name": "Hindalco Industries", "symbol": "HINDALCO.NS", "sector": "Metals"},
-
-    {"name": "Adani Enterprises", "symbol": "ADANIENT.NS", "sector": "Diversified"},
-    {"name": "Adani Ports", "symbol": "ADANIPORTS.NS", "sector": "Ports"},
-]
+    
