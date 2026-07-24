c04850297c203515714c5d96868f5e7a7a8e8d9a
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-24 22:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -902,12 +902,6 @@ with tab5:
 
     st.success("MarketVerse AI v3.0 RC")
 
-    🛡️ Project Checker
-    ────────────────────────
-    Health Score : 95%
-    Status       : GOOD
-
-    [ 🔍 Check Project ]
 
 
 # ==========================================
