849ab12eda3c5ca4885ec56f035b6a33b7d97683
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-07-26 19:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1135,6 +1135,7 @@ if st.session_state.generated_output:
 
 
 
+
 import os
 import pandas as pd
 import streamlit as st
@@ -1202,3 +1203,4 @@ if st.button("Start Scanning & Generate Report"):
                 col1, col2 = st.columns(2)
                 col1.metric("Total Successful Files", success_count)
                 col2.metric("Total Failed Files", failed_count)
+
